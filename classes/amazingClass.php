d267085cb2eb06c9648306ed546ce37c3f165eb2
Aare Laanesaar <woozy014@hotmail.com>
2011-09-30 23:34:02 +0100

    Opening of a new branch

---
+++ classes/amazingClass.php
@@ -0,0 +1,21 @@
+<?php 
+
+class AmazingClass {
+
+	public $weight;
+	public $weightUnit;
+	public $height;
+	public $weight;
+	
+public function __construct() {
+	
+$this->weight = 80;
+$this->weightUnit = "kg";
+$this->height = 185;
+$this->heightUnit = "cm";
+
+}
+
+}
+
+?>
---
+++ classes/ideas/amazingFeature.php
@@ -0,0 +1,9 @@
+<?php 
+
+class amazingFeature {
+	
+	public static $featureList = array('commenting', 'testing', 'commiting');
+
+}
+
+?>
