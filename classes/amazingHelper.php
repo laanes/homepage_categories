0afbc032e9bcee2759d4a4d9202936a67b0fa0a7
Aare Laanesaar <woozy014@hotmail.com>
2011-10-01 16:33:38 +0100

    are we still doing it right?

---
+++ classes/amazingHelper.php
@@ -0,0 +1,17 @@
+<?php 
+
+class amazingHelper {
+	
+	public static (boolean) $legDefence;
+	public static (boolean) $armDefence;
+
+	public function __construct() {
+		
+	$this->legDefence = true;
+	$this->armDefence = false;
+
+	}
+
+}
+
+?>
